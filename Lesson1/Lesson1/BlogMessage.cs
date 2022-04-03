e7277b4d0762c02170757d17cdc4a3a6b4395212
Pablito Verde <pgreeny@mail.ru>
2022-04-03 22:08:00 +0300

    Урок1

---
+++ Lesson1/Lesson1/BlogMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    public class BlogMessage
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public BlogMessage()
+        {
+
+        }
+    }
+}
