c12642e41ac8fc3f48126dec1107aed815c70eb6
Pablito Verde <pgreeny@mail.ru>
2022-04-22 22:45:14 +0300

    Lesson2

---
+++ Timesheets/Timesheets/Services/InvoiceService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Timesheets.Controllers.Models;
+using Timesheets.DAL.Entities;
+
+namespace Timesheets.Services
+{
+    public class InvoiceService : IService<InvoiceDto>
+    {
+        public Task Create(InvoiceDto item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<InvoiceDto> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<ContractEntity>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(InvoiceDto item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
